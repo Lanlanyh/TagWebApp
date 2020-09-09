74c86d4efb53bb1e8a12c4585a411965d3063acd
yla627 <yihan_lan@sfu.ca>
2020-09-09 11:54:10 +0800

    Updates

---
+++ precache-manifest.254af5273eec56d76033b4c89b7bddf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d236509c3782cebe0396126821084e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0b2bac65a5a9784f37",
+    "url": "/static/css/main.2345dece.chunk.css"
+  },
+  {
+    "revision": "c384fbdb7effd0898307",
+    "url": "/static/js/2.535e2643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.535e2643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0b2bac65a5a9784f37",
+    "url": "/static/js/main.0bf48abf.chunk.js"
+  },
+  {
+    "revision": "202fce87607a16d29b82",
+    "url": "/static/js/runtime-main.57e54692.js"
+  }
+]);
